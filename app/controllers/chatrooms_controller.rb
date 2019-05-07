8e81dd6f4bc29499402b6265fcd35eae00878256
DustinMiner <dustinjminer@outlook.com>
2019-05-06 20:38:35 -0500

    Fixed chatroom controller issue

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,8 +1,8 @@
 class ChatroomsController < ApplicationController
-  class ChatroomsController < ApplicationController
   before_action :authenticate_user!
   def show
     @messages = Message.all
+   end
   end
-end
+
 
